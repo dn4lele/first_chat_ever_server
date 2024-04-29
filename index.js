61d9cbc1ba285c31b386b554d7f9c15bcac39ae0
dn4lele <danielmaier4312@gmail.com>
2024-04-29 22:46:30 +0300

    add list of online people

--- index.js
+++ index.js
@@ -8,19 +8,41 @@ const app = express();
 const server = http.createServer(app);
 
 const io = socket(server, { cors: { origin: "*" } });
+let accounts = [];
 
 io.on("connection", (socket) => {
-  socket.on("connect", () => {
-    console.log("User connected", socket.id);
+  socket.on("getPeople", (nothing) => {
+    console.log("getPeople", accounts);
+    io.emit("getPeople", accounts);
   });
 
   socket.on("message", (message) => {
-    console.log(message);
     io.emit("message", message);
+
+    try {
+      if (message.user != null) {
+        console.log("User connected", message.user.FirstName);
+        let user = message.user;
+        accounts.push({ id: socket.id, user });
+        io.emit("getPeople", accounts);
+      }
+    } catch (e) {
+      console.log(e);
+    }
   });
 
   socket.on("disconnect", (data) => {
     console.log("User disconnected", socket.id);
+
+    let accountobj = accounts.find((account) => account.id == socket.id);
+    let username = accountobj.user.FirstName;
+    io.emit("message", {
+      message: username + " has disconnected!",
+      type: "disconnectuser",
+    });
+
+    accounts = accounts.filter((account) => account.id != socket.id);
+    io.emit("getPeople", accounts);
   });
 });
 
